e782b1e645a5308b933a8314c784ce35492e72ba
xunziheng <xunziheng88@163.com>
2022-07-26 16:41:10 +0800

    initial

--- src/index.js
+++ src/index.js
@@ -3,37 +3,35 @@ import logoImg from './assets/logo.png';
 
 class MyGame extends Phaser.Scene
 {
-    constructor ()
-    {
-        super();
-    }
+  constructor () {
+    super();
+  }
 
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+  preload () {
+    this.load.image('logo', logoImg);
+  }
+    
+  create ()
+  {
+    const logo = this.add.image(400, 150, 'logo');
+  
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1
+    });
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  scene: MyGame
 };
 
 const game = new Phaser.Game(config);
